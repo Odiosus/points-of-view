143f5f5c566520dd736922d7fe8a8bd25b0e7103
Maksim Suchkov <96449353+Odiosus@users.noreply.github.com>
2024-03-27 23:53:11 +0300

    urls comment

--- povsite/povsite/urls.py
+++ povsite/povsite/urls.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
+from froala_editor import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('froala_editor/',include('froala_editor.urls'))
 ]
 
 
