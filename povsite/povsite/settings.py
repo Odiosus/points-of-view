16ce1f044099da263f1197af6720b301a64e0447
Maksim Suchkov <96449353+Odiosus@users.noreply.github.com>
2024-03-27 20:54:20 +0300

    update settings froala

--- povsite/povsite/settings.py
+++ povsite/povsite/settings.py
@@ -104,6 +104,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+FROALA_EDITOR_OPTIONS = {
+    'language': 'ru',
+}
+
+FROALA_EDITOR_PLUGINS = ('align', 'char_counter', 'code_beautifier', 'code_view', 'colors', 'draggable', 'emoticons',
+                         'entities', 'file', 'font_family', 'font_size', 'fullscreen', 'image_manager', 'image',
+                         'inline_style', 'line_breaker', 'link', 'lists', 'paragraph_format', 'paragraph_style',
+                         'quick_insert', 'quote', 'save', 'table', 'url', 'video')
+
+FROALA_UPLOAD_PATH = 'MEDIA_ROOT'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
