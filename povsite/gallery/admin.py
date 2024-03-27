72f0d953646aa4067dc8db10fa1d1c459cfb45a4
Maksim Suchkov <96449353+Odiosus@users.noreply.github.com>
2024-03-27 16:41:27 +0300

    update admin

--- povsite/gallery/admin.py
+++ povsite/gallery/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from .models import Gallery
 
-admin.site.register(Gallery)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_display_links = ('name',)
+    search_fields = ['name']
+
+
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'content_text', 'category', 'publication_date')
+    list_display_links = ('title', 'content_text')
+    list_filter = ('content_text', 'publication_date',)
+    search_fields = ['user', 'title', 'content_text']
+    ordering = ['-publication_date']
