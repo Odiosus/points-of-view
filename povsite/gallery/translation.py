f1ba258f0ba4abfbeb409e8ca9c6168bcf5885fb
Suchkov Maksim <barbell.wire0b@icloud.com>
2024-03-29 12:25:30 +0300

    model Author register translation.py

--- povsite/gallery/translation.py
+++ povsite/gallery/translation.py
@@ -1,7 +1,12 @@
-from .models import Gallery
+from .models import Gallery, Author
 from modeltranslation.translator import register, TranslationOptions
 
 
 @register(Gallery)
 class GalleryTranslationOptions(TranslationOptions):
-    fields = ('title', 'content_picture', )
+    fields = ('title', 'content_picture',)
+
+
+@register(Author)
+class AuthorTranslationOptions(TranslationOptions):
+    fields = ('name', 'surname', 'brand_name',)  # TODO 'patronymic', 'email', 'phone'?
