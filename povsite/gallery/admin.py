30e7c4515f348da84eef8f82e2915d1b5556c2ed
Suchkov Maksim <barbell.wire0b@icloud.com>
2024-03-29 14:21:37 +0300

    update admin-panel Author

--- povsite/gallery/admin.py
+++ povsite/gallery/admin.py
@@ -8,12 +8,13 @@ from modeltranslation.admin import TranslationAdmin
 @admin.register(Gallery)
 class GalleryAdmin(TranslationAdmin, admin.ModelAdmin):
     list_display = (
-        'user', 'title', 'author', 'is_published', 'content_text', 'get_html_photo', 'content', 'publication_date', 'publication_update')
+        'user', 'title', 'author', 'is_published', 'content_text', 'get_html_photo', 'content', 'publication_date',
+        'publication_update')
     list_display_links = ('title',)
     list_filter = ('user', 'author', 'publication_date', 'publication_update')
     search_fields = ['user', 'author', 'title', 'content_text']
     ordering = ['-publication_date']
-    fields = ('user',  'title', 'author', 'content_text', 'content_picture', 'get_html_photo', 'is_published')
+    fields = ('user', 'title', 'author', 'content_text', 'content_picture', 'get_html_photo', 'is_published')
     readonly_fields = ('publication_date', 'publication_update', 'get_html_photo')
     save_on_top = True
 
@@ -26,4 +27,17 @@ class GalleryAdmin(TranslationAdmin, admin.ModelAdmin):
 
 @admin.register(Author)
 class AuthorAdmin(TranslationAdmin, admin.ModelAdmin):
-    list_display = ('name', 'surname', 'brand_name', 'email', 'phone')
+    list_display = ('get_html_photo', 'name', 'surname', 'brand_name', 'email', 'phone', 'biography')
+    list_display_links = ('name', 'surname', 'brand_name')
+    list_filter = ('surname', 'brand_name')
+    search_fields = ['name', 'surname', 'brand_name', 'biography', 'email', 'phone']
+    ordering = ['-time_add']
+    fields = ('name', 'surname', 'patronymic', 'brand_name', 'email', 'phone', 'photo', 'get_html_photo', 'biography')
+    readonly_fields = ('time_add', 'time_update', 'get_html_photo')
+    save_on_top = True
+
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f"<img src='{object.photo.url}' width=50>")
+
+    get_html_photo.short_description = 'Фото'
