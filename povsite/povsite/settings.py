0fa414e542044a5aa11ac81494c1841f220e5183
Roman <maksudov.rr@yandex.ru>
2024-04-13 13:57:28 +0300

    1.1

--- povsite/povsite/settings.py
+++ povsite/povsite/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gallery.apps.GalleryConfig',
+    'frontend', # enable the frontend app
 ]
 
 MIDDLEWARE = [
